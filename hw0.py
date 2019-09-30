9c10423cb5197a47ccae739b43b6f39153a46eb4
Rachel Rodney <rhrodney@uw.edu>
2019-09-30 14:24:59 -0700

    first push hw0

--- hw0.py
+++ hw0.py
@@ -1,20 +1,20 @@
 # This file, with a .py extension, contains a Python Program
 # Lines that start with the hash sign are ignore by Python.
-# they are used for comments. 
+# they are used for comments.
 #
 # To get started, we'll be using terminal to run our first
 # Python programs this quarter. Terminal is an app that lets
 # you type commands (at the command line or command prompt).
 #
 # This can be frustrating at first, as you need to remember
-# or be able to look up what command to use. However, it 
+# or be able to look up what command to use. However, it
 # can be much more expressive and flexible than a graphical
 # interface, because the vocabulary of possible commands is
 # much greater than can be shown in a graphical interface.
 #
 # Step 1: To run this program, in a terminal window, type:
 #
-# cd ~/wherever your homework is stored 
+# cd ~/wherever your homework is stored
 #
 # and then type "py -3.7 hw0.py" if you are using Windows
 # or type "python3 hw0.py" if you are using MacOS
@@ -36,7 +36,7 @@ def hello():
 
 hello()
 print("... Let's say that again... \n")
-hello()
+
 
 # Step 2: Now try deleting the second hello(). Save the file.
 # Run the program again to see the results.
@@ -44,6 +44,7 @@ hello()
 # Step 3: Now insert "hello()" back into the editor buffer
 # below this line. Try using the auto-complete feature. After
 # you type "hel", possible completions should appear.
+hello()
 # Use the arrow keys or the mouse to select, and hit enter.
 
 # Save the file. Run it again to see the results.
@@ -53,13 +54,13 @@ hello()
 # in the values for length, width, height, and your name.
 # Then save and run the program again.
 
-# length = 0
-# width = 0
-# height = 0
+length = 12
+width = 4
+height = 36
 #
-# me = "<your name here>"
-# print("Volume =", width * length * height)
-# print("My name is", me)
+me = "<Rachel>"
+print("Volume =", width * length * height)
+print("My name is", me)
 
 # Step 5: You can also run Python programs in PyCharm as well.
 # In PyCharm, click File and then New Project. Make sure the
